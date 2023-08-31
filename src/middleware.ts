4b769d2b6aac9ddcb5d24b2b3d82adc81111b6bd
GUSILLUS <zebela2002@gmail.com~>
2023-08-31 22:15:01 +0300

    feat: middleware improve

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  callbacks: {
+    authorized({ token }) {
+      return !!token;
+    },
+  },
+});
 
 export const config = { matcher: ['/formik', '/i18next', '/rtkq', '/profile'] };
