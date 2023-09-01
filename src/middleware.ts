6b88d9de9acd88203bcbce05b5d0f5e601f37071
GUSILLUS <zebela2002@gmail.com~>
2023-09-01 13:08:29 +0300

    feat: middleware setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
 import { withAuth } from 'next-auth/middleware';
 
+// More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   callbacks: {
-    authorized: params => {
-      const { token } = params;
+    authorized({ req, token }) {
+      if (req.nextUrl.pathname === '/') return true;
       return !!token;
     },
   },
 });
 
-export const config = { matcher: ['/formik', '/i18next', '/rtkq', '/profile'] };
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)'],
+};
